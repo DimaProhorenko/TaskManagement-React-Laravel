59c661f9dcb50f92130a27cdcf7cb2a67763b69f
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-05-14 10:33:40 +0300

    REFACTOR Table headings

---
+++ resources/js/Components/TableHeading.jsx
@@ -0,0 +1,45 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/16/solid";
+import React from "react";
+
+const TableHeading = ({
+    children,
+    name,
+    sortable = true,
+    sortDirection = null,
+    sortField = null,
+    handleSort = () => {},
+}) => {
+    return (
+        <th
+            onClick={(e) => handleSort(name)}
+            className="px-3 py-3 cursor-pointer hover:underline "
+        >
+            {!sortable && children}
+            {sortable && (
+                <div className="flex items-center gap-1">
+                    <span>{children}</span>
+                    <div className="text-slate-400">
+                        <ChevronUpIcon
+                            className={
+                                "w-4 " +
+                                (sortField === name && sortDirection === "asc"
+                                    ? "text-white"
+                                    : "")
+                            }
+                        />
+                        <ChevronDownIcon
+                            className={
+                                "w-4 -mt-2 " +
+                                (sortField === name && sortDirection === "desc"
+                                    ? "text-white"
+                                    : "")
+                            }
+                        />
+                    </div>
+                </div>
+            )}
+        </th>
+    );
+};
+
+export default TableHeading;
--- resources/js/Pages/Project/shared/List.jsx
+++ resources/js/Pages/Project/shared/List.jsx
@@ -1,8 +1,10 @@
 import SelectInput from "@/Components/SelectInput";
 import TextInput from "@/Components/TextInput";
 import { router } from "@inertiajs/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/16/solid";
 import React from "react";
 import Item from "./Item";
+import TableHeading from "@/Components/TableHeading";
 
 const List = ({ projects, queryParams = null }) => {
     queryParams = queryParams || {};
@@ -38,103 +40,103 @@ const List = ({ projects, queryParams = null }) => {
         router.get(route("project.index", queryParams));
     };
     return (
-        <table className="table-auto">
-            <thead className="border-b border-slate-200 bg-slate-700">
-                <tr className="text-nowrap">
-                    <th
-                        onClick={(e) => handleSortChanged("id")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        ID
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("image_path")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Image
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("name")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Name
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("status")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Status
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("created_at")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Create Date
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("due_date")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Due Date
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("created_by")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Created By
-                    </th>
-                    <th
-                        onClick={(e) => handleSortChanged("")}
-                        className="px-3 py-3 cursor-pointer hover:underline"
-                    >
-                        Actions
-                    </th>
-                </tr>
-            </thead>
-            <thead className="border-b border-slate-200 bg-slate-700">
-                <tr className="text-nowrap">
-                    <th className="px-3 py-3"></th>
-                    <th className="px-3 py-3"></th>
-                    <th className="px-3 py-3">
-                        <TextInput
-                            placeholder="Project name"
-                            className="w-full"
-                            defaultValue={queryParams.name || ""}
-                            onBlur={(e) =>
-                                handleSearchFieldChange("name", e.target.value)
-                            }
-                            onKeyPress={(e) => handleKeyPress("name", e)}
-                        />
-                    </th>
-                    <th className="px-3 py-3">
-                        <SelectInput
-                            className="w-full"
-                            defaultValue={queryParams.status}
-                            onChange={(e) =>
-                                handleSearchFieldChange(
-                                    "status",
-                                    e.target.value
-                                )
-                            }
+        <div className="overflow-auto">
+            <table className="table-auto">
+                <thead className="border-b border-slate-200 bg-slate-700">
+                    <tr className="text-nowrap">
+                        <TableHeading
+                            name="id"
+                            sortField={queryParams.sortField}
+                            sortDirection={queryParams.sortDirection}
+                            handleSort={handleSortChanged}
                         >
-                            <option>Select Status</option>
-                            <option value={"pending"}>Pending</option>
-                            <option value="in_progress">In Progress</option>
-                            <option value="completed">Completed</option>
-                        </SelectInput>
-                    </th>
-                    <th className="px-3 py-3"></th>
-                    <th className="px-3 py-3"></th>
-                    <th className="px-3 py-3"></th>
-                    <th className="px-3 py-3"></th>
-                </tr>
-            </thead>
-            <tbody>
-                {projects.data.map((project) => (
-                    <Item project={project} key={project.id} />
-                ))}
-            </tbody>
-        </table>
+                            ID
+                        </TableHeading>
+                        <th className="px-3 py-3">Image</th>
+                        <TableHeading
+                            name="name"
+                            sortField={queryParams.sortField}
+                            sortDirection={queryParams.sortDirection}
+                            handleSort={handleSortChanged}
+                        >
+                            Name
+                        </TableHeading>
+                        <TableHeading
+                            name="status"
+                            sortField={queryParams.sortField}
+                            sortDirection={queryParams.sortDirection}
+                            handleSort={handleSortChanged}
+                        >
+                            Status
+                        </TableHeading>
+                        <TableHeading
+                            name="created_at"
+                            sortField={queryParams.sortField}
+                            sortDirection={queryParams.sortDirection}
+                            handleSort={handleSortChanged}
+                        >
+                            Create Date
+                        </TableHeading>
+                        <TableHeading
+                            name="due_date"
+                            sortField={queryParams.sortField}
+                            sortDirection={queryParams.sortDirection}
+                            handleSort={handleSortChanged}
+                        >
+                            Due Date
+                        </TableHeading>
+                        <th className="px-3 py-3">Created By</th>
+                        <th className="px-3 py-3">Actions</th>
+                    </tr>
+                </thead>
+                <thead className="border-b border-slate-200 bg-slate-700">
+                    <tr className="text-nowrap">
+                        <th className="px-3 py-3"></th>
+                        <th className="px-3 py-3"></th>
+                        <th className="px-3 py-3">
+                            <TextInput
+                                placeholder="Project name"
+                                className="w-full"
+                                defaultValue={queryParams.name || ""}
+                                onBlur={(e) =>
+                                    handleSearchFieldChange(
+                                        "name",
+                                        e.target.value
+                                    )
+                                }
+                                onKeyPress={(e) => handleKeyPress("name", e)}
+                            />
+                        </th>
+                        <th className="px-3 py-3">
+                            <SelectInput
+                                className="w-full"
+                                defaultValue={queryParams.status}
+                                onChange={(e) =>
+                                    handleSearchFieldChange(
+                                        "status",
+                                        e.target.value
+                                    )
+                                }
+                            >
+                                <option>Select Status</option>
+                                <option value={"pending"}>Pending</option>
+                                <option value="in_progress">In Progress</option>
+                                <option value="completed">Completed</option>
+                            </SelectInput>
+                        </th>
+                        <th className="px-3 py-3"></th>
+                        <th className="px-3 py-3"></th>
+                        <th className="px-3 py-3"></th>
+                        <th className="px-3 py-3"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {projects.data.map((project) => (
+                        <Item project={project} key={project.id} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
